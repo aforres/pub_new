2c3296cdeb9613ad7a4f92037a8c3e25e6604352
aforres <aforres@users.noreply.github.com>
2024-06-29 14:16:39 +1000

    Add files via upload

---
+++ test_QRCdecoder.py
@@ -0,0 +1,24 @@
+# Import Library
+import streamlit as st
+import cv2
+
+
+# Name of the QR Code Image file
+filename = "C:\\Users\\aforr\\Thonny\\MM\\image_folder\\generate_image_20240629-113327.png"
+
+# read the QRCODE image
+image = cv2.imread(filename)
+
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+
+# detect and decode
+data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+
+# if there is a QR code
+# print the data
+if vertices_array is not None:
+  st.write("QRCode data:")
+  st.write(data)
+else:
+  st.write("There was some error")
